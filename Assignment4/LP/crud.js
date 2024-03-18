c2b08eaca1e80ced99b2887dbb970af720603fbf
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-03-19 02:23:15 +0500

    Re-structured

---
+++ Assignment4/LP/crud.js
@@ -0,0 +1,111 @@
+var selectedRow = null;
+
+
+function showAlert(message, className)
+{
+    const div = document.createElement("div");
+    div.className = `alert alert-${className}`;
+
+    div.appendChild(document.createTextNode(message));
+    const container = document.querySelector(".container");
+    const main = document.querySelector(".main");
+    container.insertBefore(div, main);
+
+    setTimeout(() =>
+        document.querySelector(".alert").remove()
+    , 3000);
+}
+
+//Clear All Fields
+function clearFields()
+{
+        document.querySelector("#firstName").value = "";
+        document.querySelector("#lastName").value = "";
+        document.querySelector("#rollNo").value =
+         "";
+}
+
+//Add Data
+
+document.querySelector("#student-form").addEventListener("submit", (e) =>
+{
+e.preventDefault();
+
+//GET VALUES
+const firstName = document.querySelector("#firstName").value;
+
+const lastName = document.querySelector("#lastName").value;
+
+const rollNo = document.querySelector("#rollNo").value;
+
+//Validate
+
+if(firstName =="" || lastName =="" || rollNo == "")
+{
+    showAlert("Please fill in all fields", "danger");
+} else
+{
+    if(selectedRow == null)
+    {
+        const list = document.querySelector("#student-list");
+        const row = document.createElement("tr");
+
+        row.innerHTML = `
+        <td>${firstName}</td> 
+        <td>${lastName}</td>
+        <td>${rollNo}</td>
+        <td>
+        <a href="#" class="btn btn-warning btn-sm edit">Edit</a>
+        <a href="#" class="btn btn-danger
+        btn-sm delete">Delete</a>
+        `;
+
+        list.appendChild(row);
+        selectedRow = null;
+        showAlert("Student Added", "success");
+    }
+    else
+    {
+        selectedRow.children[0].textContent = firstName;
+        selectedRow.children[1].textContent = lastName;
+        selectedRow.children[2].textContent = 
+        rollNo;
+        selectedRow = null;
+        showAlert("Student Info Updated", "info");
+    }
+
+    clearFields();
+}
+});
+
+//Edit Data
+
+document.querySelector("#student-list").addEventListener("click", (e) =>
+{
+    target = e.target;
+    if(target.classList.contains("edit"))
+    {
+        selectedRow = target.parentElement.parentElement;
+        document.querySelector("#firstName").value =
+        selectedRow.children[0].textContent;
+        document.querySelector("#lastName").value =
+        selectedRow.children[1].textContent;
+        document.querySelector("#rollNo").value =
+        selectedRow.children[2].textContent;
+    }
+});
+
+
+//Delete Data
+
+
+document.querySelector("#student-list").addEventListener("click", (e) =>
+{
+    target = e.target;
+    if(target.classList.contains("delete"))
+    {
+        target.parentElement.parentElement.remove();
+        showAlert("Student Deleted", "danger");
+    }
+   
+});
