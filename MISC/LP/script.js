79c0ab4f8cc50f4b2d546d6fb68d203dad0709d0
Saad Suleman <91990781+SAADSULEMAN053@users.noreply.github.com>
2024-04-01 13:36:47 +0500

    Mid Term Lab Exam

--- MISC/LP/script.js
+++ MISC/LP/script.js
@@ -16,3 +16,26 @@ navToggle.addEventListener("click", ()=> {
         navToggle.setAttribute('aria-expanded',false);
     }
 })
+
+document.addEventListener("DOMContentLoaded", function () {
+    const logo = document.querySelector('.logo');
+    const textUnderLogo = document.createElement('p');
+    textUnderLogo.classList.add('text-under-logo');
+    logo.parentNode.insertBefore(textUnderLogo, logo.nextSibling);
+
+    const images = document.querySelectorAll('.container img');
+    const logoText = document.querySelector('.text-under-logo');
+
+    images.forEach(image => {
+        image.addEventListener('mouseenter', () => {
+            const imageText = image.getAttribute('alt');
+            logoText.textContent = imageText;
+            logoText.style.display = 'block';
+        });
+
+        image.addEventListener('mouseleave', () => {
+            logoText.style.display = 'none';
+        });
+    });
+});
+
